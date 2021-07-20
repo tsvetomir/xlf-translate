950ac27f51306f0fff5d34741ee3ce01487a3215
Tsvetomir Tsonev <tsvetomir.tsonev@progress.com>
2021-07-20 14:25:01 +0300

    chore: update dependencies

--- spec/translate.spec.js
+++ spec/translate.spec.js
@@ -1,21 +1,20 @@
 'use strict';
 
-const yaml = require('js-yaml');
 const fs = require('fs');
 const cheerio = require('cheerio');
-const translate = require('../translate');
+const translate = require('../src/translate');
 
 const lang = {
-    "localized": {
-        "component": {
-            "hello": "Bonjour!",
-            "goodbye": "Au Revoir!",
-            "1": "un"
+    'localized': {
+        'component': {
+            'hello': 'Bonjour!',
+            'goodbye': 'Au Revoir!',
+            '1': 'un'
         }
     }
 };
 
-describe("translate", function() {
+describe('translate', function() {
     let messages;
     let units;
 
@@ -27,15 +26,15 @@ describe("translate", function() {
         units = messages('trans-unit');
     });
 
-    it("fills tagged units", function() {
+    it('fills tagged units', function() {
         translate(messages, lang);
 
         expect(target(1)).toBe(lang.localized.component.hello);
         expect(target(2)).toBe(lang.localized.component.goodbye);
-        expect(target(3)).toBe(lang.localized.component["1"]);
+        expect(target(3)).toBe(lang.localized.component['1']);
     });
 
-    it("skips translated tagged units", function() {
+    it('skips translated tagged units', function() {
         target(1, 'Foo');
 
         translate(messages, lang);
@@ -43,7 +42,7 @@ describe("translate", function() {
         expect(target(1)).toBe('Foo');
     });
 
-    it("overwrites translated tagged units with force", function() {
+    it('overwrites translated tagged units with force', function() {
         target(1, 'Foo');
 
         translate(messages, lang, true);
@@ -51,32 +50,32 @@ describe("translate", function() {
         expect(target(1)).toBe(lang.localized.component.hello);
     });
 
-    it("skips regular units", function() {
+    it('skips regular units', function() {
         translate(messages, lang);
 
         expect(target(0)).toBe('');
     });
 
-    it("skips units with interpolations", function() {
+    it('skips units with interpolations', function() {
         translate(messages, lang);
 
         expect(target(4)).toBe('');
     });
 
-    it("outputs interpolations", function() {
+    it('outputs interpolations', function() {
         translate(messages, lang);
 
         expect(units.eq(4).find('source').html()).toBe('Créé par <x id="INTERPOLATION"/> le <x id="INTERPOLATION_1"/>');
     });
 
-    describe("with no target", function() {
+    describe('with no target', function() {
         beforeEach(() => {
             const messageData = fs.readFileSync('./sample/messages.no-target.xlf', { encoding: 'utf-8' });
             messages = cheerio.load(messageData, { xmlMode: true, decodeEntities: false });
             units = messages('trans-unit');
         });
 
-        it("fills tagged units", function() {
+        it('fills tagged units', function() {
             translate(messages, lang);
 
             expect(target(1)).toBe(lang.localized.component.hello);
